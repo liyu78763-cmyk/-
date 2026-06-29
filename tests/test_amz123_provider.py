ee5b1487f9d336477f990487ea2937a6bd155816
liyu78763-cmyk <liyu78763@gmail.com>
2026-06-29 15:04:23 +0800

    Cover AMZ123 North America filtering

--- tests/test_amz123_provider.py
+++ tests/test_amz123_provider.py
@@ -14,6 +14,5 @@ def test_parse_amz123_headlines_keeps_relevant_article_links() -> None:
         "2026年美国Prime Day首日销售额达83亿美元",
         "26年加拿大Prime Day总消费将达54亿加元，购物意愿提升",
         "亚马逊MFN备货时间新规6月29日生效，严禁虚报时效",
-        "亚马逊自有品牌Amazon Basics进军巴西市场",
     ]
     assert all(headline.url.startswith("https://www.amz123.com/") for headline in headlines)
